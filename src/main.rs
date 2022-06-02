1b10d7fec25065ab7655203743f35fcd86e123ca
Mimi <miraclef60@gmail.com>
2022-06-02 13:34:46 +0100

    Added new route handlers and tested async/await functionalities and sleep timer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::tokio::time::{sleep, Duration};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, New Journey started!"
+}
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+//A sleep route test
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds before execution", seconds)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, hello, delay])
 }
