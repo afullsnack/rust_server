1608bb80ea010959bd78f7405d7b269dd5c447bf
Miracle Friday <miraclef60@gmail.com>
2023-09-02 00:31:50 +0100

    fix: fixed return string of delayed function

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,18 @@ fn hello(name: String, age: u8) -> String {
 #[get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
     sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds before execution", seconds)
+
+    let loud = { String::from("This is a returned string") };
+
+    format!(
+        "Waited for {} seconds before execution, then::: -> {}",
+        seconds, loud
+    )
 }
 
 #[get("/pet/<pet_type>/<breed>")]
 async fn get_pet(pet_type: String, breed: String) -> String {
-    format!("Your pet is a {}, and its breed is {}", pet_type, breed)
+    format!("Our pet is a {}, and its breed is {}", pet_type, breed)
 }
 
 #[launch]
